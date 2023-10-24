c3be47fb9857ef053c0db3393fc99692d135be0b
Homer Martillano <hamartillano@gmail.com>
2023-10-24 17:28:35 +1100

    developed in game UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -21,4 +21,9 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene("PacMan");
     }
+
+    public void OnClickExit()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
 }
